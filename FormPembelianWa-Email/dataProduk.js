c1ff2be86fce92d6fead2132df40c37d55a69ea6
MAGIC Reincarnated <139699919+MagicReincarnation@users.noreply.github.com>
2024-03-19 22:33:29 +0700

    Create dataProduk.js

---
+++ FormPembelianWa-Email/dataProduk.js
@@ -0,0 +1,26 @@
+var produk = {
+  "1": {
+    gambarproduk: {
+      url: "https://w7.pngwing.com/pngs/624/612/png-transparent-blend-s-anime-manga-anime-child-face-black-hair-thumbnail.png"
+    },
+    produk: {
+      nama: "Sepatu"
+    },
+    harga: {
+      Rp: "30.000",
+      dollar: "3"
+    }
+  },
+  "2": {
+    gambarproduk: {
+      url: "https://c4.wallpaperflare.com/wallpaper/757/911/790/women-anime-girls-brunette-cat-wallpaper-preview.jpg"
+    },
+    produk: {
+      nama: "Manga"
+    },
+    harga: {
+      Rp: "70.000",
+      dollar: "6"
+    }
+  }
+};
